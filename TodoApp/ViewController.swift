07a3872c5244c96dc366e9109a47c542c929d3c7
tsuyoshi777 <t12619tt@gmail.com>
2018-03-07 22:37:46 +0900

    Make tableView IBoutlet

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var todo = [Todo]()
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

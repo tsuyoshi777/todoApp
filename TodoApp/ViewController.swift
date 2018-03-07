78cde1aa7334f30b811de90fe06d3f0a1d9efddf
tsuyoshi777 <t12619tt@gmail.com>
2018-03-08 04:44:16 +0900

    Add function about tableView and function about delete in Firebase

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -72,6 +72,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         })
     }
     
+    func deleteTodo(_ location: Int) {
+        let ref = Database.database().reference()
+        var i = 0
+        ref.child("TodoItems").observeSingleEvent(of: .value, with: { (snapshot) in
+            for item in snapshot.children {
+                let child = item as! DataSnapshot
+                let uniqueId = child.key
+                if i == location {
+                    ref.ref.child("TodoItems/\(uniqueId)").removeValue()
+                }
+                i += 1
+                
+            }
+        })
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todo.count
     }
@@ -103,5 +119,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let location = self.todo.count - indexPath.row
         updateTodo(location)
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            todo.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+            let location = self.todo.count - indexPath.row
+            deleteTodo(location)
+        }
+    }
 }
-

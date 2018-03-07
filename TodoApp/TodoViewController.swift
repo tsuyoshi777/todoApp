86dca940e0ba6f80f3f0978155f8225fc8d04cc7
tsuyoshi777 <t12619tt@gmail.com>
2018-03-08 07:13:31 +0900

    Transfer logic not dealing with ui to TodoViewModel.swift

--- TodoApp/TodoViewController.swift
+++ TodoApp/TodoViewController.swift
@@ -12,7 +12,6 @@ import FirebaseDatabase
 class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     let viewModel: TodoViewModel = TodoViewModel.sharedModel
-    var todo = [Todo]()
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -36,8 +35,8 @@ class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDe
             let todoTitle = viewModel.newTask
             todo.title = todoTitle
             todo.isDone = false
-            self.todo.insert(todo, at: 0)
-            createTodo(todo)
+            viewModel.todo.insert(todo, at: 0)
+            viewModel.createTodo(todo)
             
             self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableViewRowAnimation.right)
             viewModel.checkUpdate = false
@@ -48,14 +47,7 @@ class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDe
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func createTodo(_ todo: Todo) {
-        let ref = Database.database().reference()
-        let todoRef = ref.child("TodoItems").childByAutoId()
-        let newTodo = ["title": todo.title,"isDone": todo.isDone] as [String : Any]
-        todoRef.updateChildValues(newTodo)
-    }
-    
+     
     func readTodo() {
         let ref = Database.database().reference()
         ref.child("TodoItems").observeSingleEvent(of: .value, with: { (snapshot) in
@@ -65,55 +57,20 @@ class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDe
                 let todo = Todo()
                 todo.title = dic["title"] as! String
                 todo.isDone = dic["isDone"] as! Bool
-                self.todo.insert(todo, at: 0)
+                self.viewModel.todo.insert(todo, at: 0)
                 
             }
             self.tableView.reloadData()
         })
     }
     
-    func updateTodo(_ location: Int) {
-        let ref = Database.database().reference()
-        var i = 1
-        ref.child("TodoItems").observeSingleEvent(of: .value, with: { (snapshot) in
-            for item in snapshot.children {
-                let child = item as! DataSnapshot
-                let uniqueId = child.key
-                let dic = child.value as! NSDictionary
-                if i == location {
-                    let title = dic["title"] as! String
-                    let isDone = dic["isDone"] as! Bool
-                    let updateItems = ["TodoItems/\(uniqueId)":["title": title, "isDone": !isDone]]
-                    ref.updateChildValues(updateItems)
-                }
-                i += 1
-            }
-        })
-    }
-    
-    func deleteTodo(_ location: Int) {
-        let ref = Database.database().reference()
-        var i = 0
-        ref.child("TodoItems").observeSingleEvent(of: .value, with: { (snapshot) in
-            for item in snapshot.children {
-                let child = item as! DataSnapshot
-                let uniqueId = child.key
-                if i == location {
-                    ref.ref.child("TodoItems/\(uniqueId)").removeValue()
-                }
-                i += 1
-                
-            }
-        })
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todo.count
+        return viewModel.todo.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
-        let todo = self.todo[indexPath.row]
+        let todo = viewModel.todo[indexPath.row]
         cell.textLabel?.text = todo.title
         
         if todo.isDone {
@@ -126,7 +83,7 @@ class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let myTodo = todo[indexPath.row]
+        let myTodo = viewModel.todo[indexPath.row]
         
         if myTodo.isDone {
             myTodo.isDone = false
@@ -135,8 +92,8 @@ class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDe
         }
         
         tableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.fade)
-        let location = self.todo.count - indexPath.row
-        updateTodo(location)
+        let location = viewModel.todo.count - indexPath.row
+        viewModel.updateTodo(location)
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -145,10 +102,10 @@ class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCellEditingStyle.delete {
-            todo.remove(at: indexPath.row)
+            viewModel.todo.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
-            let location = self.todo.count - indexPath.row
-            deleteTodo(location)
+            let location = viewModel.todo.count - indexPath.row
+            viewModel.deleteTodo(location)
         }
     }
 }
--- TodoApp/TodoViewModel.swift
+++ TodoApp/TodoViewModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
 class TodoViewModel: NSObject {
     static let sharedModel: TodoViewModel = TodoViewModel()
@@ -14,5 +15,48 @@ class TodoViewModel: NSObject {
     var checkUpdate:Bool = false
     var isFistBoost:Bool = true
     var newTask:String = ""
+    var todo = [Todo]()
+    
+    func createTodo(_ todo: Todo) {
+        let ref = Database.database().reference()
+        let todoRef = ref.child("TodoItems").childByAutoId()
+        let newTodo = ["title": todo.title,"isDone": todo.isDone] as [String : Any]
+        todoRef.updateChildValues(newTodo)
+    }
+    
+    func updateTodo(_ location: Int) {
+        let ref = Database.database().reference()
+        var i = 1
+        ref.child("TodoItems").observeSingleEvent(of: .value, with: { (snapshot) in
+            for item in snapshot.children {
+                let child = item as! DataSnapshot
+                let uniqueId = child.key
+                let dic = child.value as! NSDictionary
+                if i == location {
+                    let title = dic["title"] as! String
+                    let isDone = dic["isDone"] as! Bool
+                    let updateItems = ["TodoItems/\(uniqueId)":["title": title, "isDone": !isDone]]
+                    ref.updateChildValues(updateItems)
+                }
+                i += 1
+            }
+        })
+    }
+    
+    func deleteTodo(_ location: Int) {
+        let ref = Database.database().reference()
+        var i = 0
+        ref.child("TodoItems").observeSingleEvent(of: .value, with: { (snapshot) in
+            for item in snapshot.children {
+                let child = item as! DataSnapshot
+                let uniqueId = child.key
+                if i == location {
+                    ref.ref.child("TodoItems/\(uniqueId)").removeValue()
+                }
+                i += 1
+                
+            }
+        })
+    }
     
 }
